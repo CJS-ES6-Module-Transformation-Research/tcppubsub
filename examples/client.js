08bf9be982214706d00ba5d3a17f33fb62d0cf17
yannick.grund@students.ffhs.ch <yannick.grund@students.ffhs.ch>
2020-01-12 12:49:18 +0100

    Add request-response pattern.

--- examples/client.js
+++ examples/client.js
@@ -1,44 +1,52 @@
 var tcppubsub = require('../lib/main')
  
+ 
 var port = 2223
 var host = 'localhost'
-
+ 
 //Create the member
 var member = new tcppubsub.Member(port, host)
-
-
+ 
+ 
 member.connect(function(){
-
+ 
     //Subscribe the topic without callback
     member.sub('app/configuration/service')
-
+ 
     // Subscribe the topic STRING or ARRAY
     member.sub('app/configuration/server', function(topic){
-
+ 
         // Publish some data STRING, OBJECT, ARRAY
         member.pub('app/configuration/server', {name: 'yamigr', b : 'Hello World'})
     })
-
+ 
+ 
     // Receive the data on the certain topic
     member.on('app/configuration/server', function(data){
-
+ 
         // Unsubscribe the topic
         member.unsub('app/configuration/server')
         console.log('rcv publish:', data)
     })
-
-
+ 
+    /******* OR *******/
+ 
+    // Receive all publishes on subscribed data
+    member.on('message', function(topic, data){
+        member.unsub('app/configuration/server')
+        console.log(topic, data)
+    })
+ 
+ 
     member.listen('app/static/config', function(data, res){
-        console.log('rcv request for static data:', data)
         res({ msg: 'Hello ' + data.name})
     })
-
+ 
+ 
     let timeout = 2000 // default 5000
-
+ 
     member.req('app/static/config', { name : 'Peter Pan'}, function(err, data, id){
-        if(err){
-            // timeout error
-        }else{
+        if(!err){
             console.log('rcv response:', data.msg)
         }
     }, timeout)
